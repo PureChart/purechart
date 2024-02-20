b3be8e056f2636dea7567bcf635d821b2311d043
GeorgeBerdovskiy <gaberdovskiy@gmail.com>
2024-02-20 13:49:55 -0800

    Add Colors to Default Styling, Fix Lollipop Chart Style Logic

--- lib/purechart/chart_helper.rb
+++ lib/purechart/chart_helper.rb
@@ -5,16 +5,24 @@ module PureChart
             default_config_path = File.join( File.dirname(__FILE__), 'styles/default.yml' )
 
             default_config_hash = YAML.load(File.read(default_config_path))
-            user_config_hash = YAML.load(File.read(File.join( File.dirname(__FILE__), 'styles/' + 'default' + '.yml' )))
+            user_config_hash = {}
 
-            if path != ""
+            if path == "professional_light"
+                # TODO - Instead of loading our own by default, try/catch to see if they defined their own
+                # style using the same name
+                style_config_path = File.join( File.dirname(__FILE__), 'styles/professional_light.yml' )
+                default_config_hash = YAML.load(File.read(style_config_path))
+            elsif path == "professional_dark"
+                style_config_path = File.join( File.dirname(__FILE__), 'styles/professional_dark.yml' )
+                default_config_hash = YAML.load(File.read(style_config_path))
+            elsif path != ""
                 # TODO - Implement better logic
-                if File.file?("styles/" + path + ".yml")
-                    user_config_hash = YAML.load(File.read("styles/" + path + ".yml"))
+                if File.file?("app/purechart/" + path + ".yml")
+                    user_config_hash = YAML.load(File.read("app/purechart/" + path + ".yml"))
                 elsif File.file?("app/purechart/" + path + ".yaml")
-                    user_config_hash = YAML.load(File.read("styles/" + path + ".yaml"))
+                    user_config_hash = YAML.load(File.read("app/purechart/" + path + ".yaml"))
                 elsif File.file?("app/purechart/" + path + ".json")
-                    user_config_hash = JSON.load(File.read("styles/" + path + ".json"))
+                    user_config_hash = JSON.load(File.read("app/purechart/" + path + ".json"))
                 else
                     raise "(PureChart) ERROR - Could not locate configuration file '" + path + ".[YML, YAML, JSON]'. Make sure this file exists in your 'app/purechart' directory."
                 end
@@ -44,8 +52,7 @@ module PureChart
         end
 
         def dot_svg_render
-            <<-SVG.strip_heredoc.html_safe
-              <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+           '''<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
                 <path d="M 10 10 H 190 V 190 H 10 L 10 10" fill="none" stroke="black"/>
                 <circle cx="15" cy="100" r="6" fill="red"/>
                 <circle cx="30" cy="60" r="6" fill="red"/>
@@ -54,10 +61,8 @@ module PureChart
                 <circle cx="160" cy="20" r="6" fill="red"/>
                 <circle cx="180" cy="150" r="6" fill="red"/>
                 <circle cx="40" cy="20" r="6" fill="red"/>
-              </svg>
-            SVG
-          end
-          
+            </svg>'''.html_safe
+        end
 
         def bar_chart
             "<div>Bar chart will be rendered here.</div>".html_safe
