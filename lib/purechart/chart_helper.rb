83f40cc08708d5adf9ddf48471e4ca7b9d332f70
Rajat Mahesh Gupta <guptarajat978@gmail.com>
2024-03-04 05:18:43 -0800

    initial commit

--- lib/purechart/chart_helper.rb
+++ lib/purechart/chart_helper.rb
@@ -89,5 +89,55 @@ module PureChart
         def dot_plot
             "<div>Dot plot will be rendered here.</div>".html_safe
         end
+
+        def line_plot(data, configuration = { axes: { vertical: "Value" } }, path="")
+            # Set default configuration file path
+            default_config_path = File.join( File.dirname(__FILE__), 'styles/default.yml' )
+
+            default_config_hash = YAML.load(File.read(default_config_path))
+            user_config_hash = {}
+            if path == "professional_light"
+                # TODO - Instead of loading our own by default, try/catch to see if they defined their own
+                # style using the same name
+                style_config_path = File.join( File.dirname(__FILE__), 'styles/professional_light.yml' )
+                default_config_hash = YAML.load(File.read(style_config_path))
+            elsif path == "professional_dark"
+                style_config_path = File.join( File.dirname(__FILE__), 'styles/professional_dark.yml' )
+                default_config_hash = YAML.load(File.read(style_config_path))
+            elsif path == "futuristic_light"
+                style_config_path = File.join( File.dirname(__FILE__), 'styles/futuristic_light.yml' )
+                default_config_hash = YAML.load(File.read(style_config_path))
+            elsif path == "futuristic_dark"
+                style_config_path = File.join( File.dirname(__FILE__), 'styles/futuristic_dark.yml' )
+                default_config_hash = YAML.load(File.read(style_config_path))
+            elsif path != ""
+                # TODO - Implement better logic
+                if File.file?("app/purechart/" + path + ".yml")
+                    user_config_hash = YAML.load(File.read("app/purechart/" + path + ".yml"))
+                elsif File.file?("app/purechart/" + path + ".yaml")
+                    user_config_hash = YAML.load(File.read("app/purechart/" + path + ".yaml"))
+                elsif File.file?("app/purechart/" + path + ".json")
+                    user_config_hash = JSON.load(File.read("app/purechart/" + path + ".json"))
+                else
+                    raise "(PureChart) ERROR - Could not locate configuration file '" + path + ".[YML, YAML, JSON]'. Make sure this file exists in your 'app/purechart' directory."
+                end
+            end
+            
+            # Merge user's configuration with default
+            style_config = default_config_hash.merge(user_config_hash)
+
+            # Format data for chart generation
+            largest_value = (data.map { |object| object[:value] }).max()
+
+            data.each do |object|
+                object[:length] = (Float(object[:value]) / largest_value) * 100
+            end
+
+            ActionController::Base.render partial: '/line_plot', locals: {
+                data: data,
+                configuration: configuration,
+                style: style_config
+            }
+        end 
     end
 end
